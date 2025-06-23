0a8121b7e31b25c55f12e6bf20548dd053c0b5ea
JP-Becker <jpbecker20@gmail.com>
2025-06-23 19:24:17 -0300

    commit inicial

---
+++ src/app.ts
@@ -0,0 +1,88 @@
+import express, { NextFunction } from "express";
+import { loadFixtures } from "./fixtures";
+import { logRequest, logResponse } from "./lib/log";
+import { userRouter } from "./router/user-router";
+import { createDatabaseConnection } from "./database";
+import  jwt  from "jsonwebtoken";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+//log requests
+app.use(logRequest);
+// tudo que usa app.use é um middleware -> é uma função que é executada antes de uma rota ser chamada
+//log responses headers
+app.use(logResponse);
+
+
+// Tratamento de erros para pre-middlewares
+app.use(
+  async (
+    error: Error,
+    req: express.Request,
+    res: express.Response,
+    next: NextFunction
+  ) => {
+    if (!error) {
+      return next();
+    }
+    errorHandler(error, req, res, next);
+  }
+);
+
+
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const {userRepository} = await createDatabaseConnection();
+    const user = await userRepository.findOne({where: {email}})
+    if (!user || !user.comparePassword(password)) {
+
+
+      
+      throw new Error('Invalid Credentials')
+    }
+    const accessToken = jwt.sign({ name: user.name, email: user.email}, 'secret');
+    res.json({ accessToken })
+});
+
+
+// Rotas da API
+app.use("", userRouter);
+
+//Tratamento de erros global da rotas da API
+app.use(errorHandler);
+
+app.listen(+PORT, "0.0.0.0", async () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  await loadFixtures();
+});
+
+function errorHandler(
+  error: Error,
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) {
+  if(!error){
+    return;
+  }
+  
+  //some errors
+
+  const errorDetails = {
+    name: error.name,
+    message: error.message,
+    stack: error.stack,
+    cause: error.cause,
+  };
+  console.error("Error details:", JSON.stringify(errorDetails, (key, value) => {
+    // Se for a stack trace, formata com quebras de linha adequadas
+    if (key === 'stack' && typeof value === 'string') {
+      return value.split('\n').map(line => line.trim());
+    }
+    return value;
+  }, 2));
+  res.status(500).send({ message: "Internal server error" });
+}
